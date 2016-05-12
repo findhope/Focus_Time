3f1631d8ee34fdbac782081ba95780eaf105799c
Kevin <findhope.china@gmail.com>
2016-05-12 16:46:19 +0800

    新建项目测试

---
+++ Focus_Time/Focus_Time/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Focus_Time
+//
+//  Created by MitnickKevin on 16/5/12.
+//  Copyright © 2016年 SWUST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Focus_Time/Focus_Time/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Focus_Time
+//
+//  Created by MitnickKevin on 16/5/12.
+//  Copyright © 2016年 SWUST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
